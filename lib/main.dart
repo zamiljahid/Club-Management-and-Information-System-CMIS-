655db42a1f0cfa6b5faecf252e5d5a0c5fed4a34
mdarise <mdalmashossenarise@gmail.com>
2024-12-06 22:07:53 +0600

    Splash Screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:club_management_and_information_system/screens/club_screen.dart';
 import 'package:club_management_and_information_system/screens/login_screeen.dart';
+import 'package:club_management_and_information_system/screens/splash_screen.dart';
 import 'package:club_management_and_information_system/shared_preference.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -28,7 +29,9 @@ class MainClassState extends State<MainClass> {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: const SplashScreen(),
     );
   }
+
+
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,8 @@
 
+import 'package:club_management_and_information_system/screens/login_screeen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,11 +11,56 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+with SingleTickerProviderStateMixin {
+  @override
+  void initState() { // Corrected here
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 5),() {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_)=>const LoginScreen())
+      );
+    }
+    );
+  }
+  @override
+  void dispose(){
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    overlays: SystemUiOverlay.values);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff8c0000), Color(0xffda2851)],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8), // Border width
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xffffffff), Color(0xffffffff)],),
+                  shape: BoxShape.circle),
+              child: ClipOval(
+                child: SizedBox.fromSize(
+                  size: Size.fromRadius(100),
+                  child: Image.asset('assets/images/appIcon.jpg', fit: BoxFit.cover),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
